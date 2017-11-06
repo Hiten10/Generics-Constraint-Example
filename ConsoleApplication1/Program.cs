f3f9a8791ffbfcf321c0806a10e240d6b84b74d1
Kumar <hitendra100282@gmail.com>
2017-11-06 18:03:38 +0530

    Random Password Generator method added

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -225,7 +225,45 @@ namespace ConsoleApplication1
 
             return null;
         }
-    }
+        
+    static string GeneratePassword(int lowercase, int uppercase, int numerics, int special) 
+    {
+    string lowers = "abcdefghijklmnopqrstuvwxyz";
+    string uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string number = "0123456789";
+    string specials = "!@^&%$#*{+}-=?_";  
+
+    Random random = new Random();
+
+    string generated = "!";
+    for (int i = 1; i <= lowercase; i++)
+        generated = generated.Insert(
+            random.Next(generated.Length), 
+            lowers[random.Next(lowers.Length - 1)].ToString()
+        );
+
+    for (int i = 1; i <= uppercase; i++)
+        generated = generated.Insert(
+            random.Next(generated.Length), 
+            uppers[random.Next(uppers.Length - 1)].ToString()
+        );
+
+    for (int i = 1; i <= numerics; i++)
+        generated = generated.Insert(
+            random.Next(generated.Length), 
+            number[random.Next(number.Length - 1)].ToString()
+        );
+        
+    for (int i = 1; i <= special; i++)
+        generated = generated.Insert(
+            random.Next(generated.Length), 
+            specials[random.Next(specials.Length - 1)].ToString()
+        );
+
+    return generated;
+
+}
+}
 
     class Test:BaseClass
     {
